690788961d378ceac1b77ad5f05d7e58df7ffbd1
KyleGodzki <kyledg01281274@gmail.com>
2026-03-17 16:58:04 +0000

    Update task2.c

--- task2.c
+++ task2.c
@@ -1,18 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+    if (c1 == 0) {
+            printf("In CHILD-1 (PID=%d): executing command %s ...\n", getpid(), argv1[0]);
+            dup2(pipe1[1], STDOUT_FILENO);
 
-int main(void) {
-    char * argv1[] = {"cat", "Makefile", 0};
-    char * argv2[] = {"head", "-4", 0};
-    char * argv3[] = {"wc", "-l", 0};
+            close(pipe1[0]);
+            close(pipe1[1]);
+            close(pipe2[0]);
+            close(pipe2[1]);
 
-    setbuf(stdout, NULL);
+            execvp(argv1[0], argv1);
+            printf("Exec failed");
+            exit(1);
+    }
 
+    pid_t c2 = fork();
+    if (c2 == 0) {
+            printf("In CHILD-2 (PID=%d): executing command %s ...\n", getpid(), argv2[0]);
 
-    return 0;
-}
+            dup2(pipe1[0], STDIN_FILENO);
+            dup2(pipe2[1], STDOUT_FILENO);
+
+            close(pipe1[0]);
+            close(pipe1[1]);
+            close(pipe2[0]);
+            close(pipe2[1]);
+
+            execvp(argv2[0], argv2);
+            printf("Exec failed");
+            exit(1);
+    }
+
+    pid_t c3 = fork();
+    if(c3 == 0) {
+            printf("In CHILD-3 (PID=%d): executing command %s ...\n", getpid(), argv3[0]);
+
+            dup2(pipe2[0], STDIN_FILENO);
+
+            close(pipe1[0]);
+            close(pipe1[1]);
+            close(pipe2[0]);
+            close(pipe2[1]);
+
+            execvp(argv3[0], argv3);
+            printf("Exec failed");
+            exit(1);
+    }
+
+    close(pipe1[0]);
+    close(pipe1[1]);
+    close(pipe2[0]);
+"task2.c" 89L, 1940B                                                                                                                                                                           69,13-20      61%
